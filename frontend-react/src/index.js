8bb27125d04e880dc54f31254d610259ef14a754
Marcelo Abiarraj <marceloat01@gmail.com>
2022-06-13 14:24:43 -0300

    Unify backend and frontend apps in monorepo

---
+++ frontend-react/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Game from './components/Game'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Game />);
+reportWebVitals();
